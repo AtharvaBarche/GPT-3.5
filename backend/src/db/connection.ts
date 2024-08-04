36276120f2be1b3f87dd069320db98119535b6f1
AtharvaBarche <barcheatharva01@gmail.com>
2024-08-04 12:36:35 +0530

    Initial commit with frontend and backend folders

---
+++ backend/src/db/connection.ts
@@ -0,0 +1,20 @@
+import { connect, disconnect } from "mongoose";
+async function connectToDatabase() {
+  try {
+    await connect("mongodb+srv://atharva:GpcrJwGmEHlzbF0E@cluster0.5gghydl.mongodb.net/chatbot?retryWrites=true&w=majority&appName=Cluster0");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Could not Connect To MongoDB");
+  }
+}
+
+async function disconnectFromDatabase() {
+  try {
+    await disconnect();
+  } catch (error) {
+    console.log(error);
+    throw new Error("Could not Disconnect From MongoDB");
+  }
+}
+
+export { connectToDatabase, disconnectFromDatabase };
